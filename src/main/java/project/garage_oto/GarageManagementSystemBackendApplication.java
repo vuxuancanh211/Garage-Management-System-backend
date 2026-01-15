c80fde1308c5f31d3e643294681325f3101ef241
vuxuancanh211 <vuxuancanh2004@gmail.com>
2026-01-15 14:20:04 +0700

    first commit

---
+++ src/main/java/project/garage_oto/GarageManagementSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package project.garage_oto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarageManagementSystemBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GarageManagementSystemBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/project/garage_oto/GarageManagementSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package project.garage_oto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarageManagementSystemBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
